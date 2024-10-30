1d0bcacf935ac85405d3d18c2f36b58a1a14bf46
Balaji B <balaji.balachandar1@gmail.com>
2024-10-30 21:53:57 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv("C:/Python/Environmental variables/.env")
+
+my_mail = "sampleforpythonmail@gmail.com"
+password = os.getenv("smtp_app_password")
+
 app = Flask(__name__)
 n_point_url = "https://api.npoint.io/c696dae3a68e24dbff46"
 
 response = requests.get(n_point_url)
-data = response.json()
+blog_data = response.json()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", blog_data = data)
+    return render_template("index.html", blog_data = blog_data)
 
 
 @app.route('/about')
@@ -22,12 +30,23 @@ def contact():
     if request.method == "GET":
         return render_template("contact.html")
     if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(my_mail, password=password)
+            connection.sendmail(from_addr=my_mail,
+                                to_addrs=my_mail,
+                                msg=f"Subject:Message from Blog\n\n"
+                                    f"Name : {data['name']}\n"
+                                    f"Email : {data['email']}\n"
+                                    f"Phone : {data['phone']}\n"
+                                    f"Message : {data['message']}")
         return render_template("contact.html",submitted = True)
 
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    for blog in data:
+    for blog in blog_data:
         if blog["id"] == post_id:
             return render_template("post.html",post = blog)
 
