148895a6a659bf3c7161b4d8f56156e65f178cc0
Balaji B <balaji.balachandar1@gmail.com>
2024-10-30 21:05:48 +0530

    initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 n_point_url = "https://api.npoint.io/c696dae3a68e24dbff46"
@@ -6,31 +6,32 @@ n_point_url = "https://api.npoint.io/c696dae3a68e24dbff46"
 response = requests.get(n_point_url)
 data = response.json()
 
-def get_post_by_id(post_id):
-    for blog in data:
-        if blog["id"] == post_id:
-            return blog
-
-
 
 @app.route('/')
 def home():
     return render_template("index.html", blog_data = data)
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+
+@app.route('/contact',methods=["POST","GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html")
+    if request.method == "POST":
+        return render_template("contact.html",submitted = True)
+
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    if get_post_by_id(post_id):
-        return render_template("post.html",post = get_post_by_id(post_id))
-    else:
-        return "Blogs not found",404
+    for blog in data:
+        if blog["id"] == post_id:
+            return render_template("post.html",post = blog)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
